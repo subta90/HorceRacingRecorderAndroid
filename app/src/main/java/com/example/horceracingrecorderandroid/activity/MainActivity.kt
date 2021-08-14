3d5d2649c5d6392345aeaabd14ef28d00a6602a3
subta90 <the.bird.nobility298@gmail.com>
2021-08-14 16:46:49 +0900

    BottomNavigationに対応するNavigationGraphを追加しとりあえず画面遷移ができるようにした

--- app/src/main/java/com/example/horceracingrecorderandroid/activity/MainActivity.kt
+++ app/src/main/java/com/example/horceracingrecorderandroid/activity/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.horceracingrecorderandroid.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.horceracingrecorderandroid.R
+import com.example.horceracingrecorderandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        val navController = findNavController(R.id.main_navigation)
+        setupWithNavController(binding.mainBottomNavigation, navController)
     }
 }
--- app/src/main/java/com/example/horceracingrecorderandroid/fragment/raceanalysis/RaceAnalysisFragment.kt
+++ app/src/main/java/com/example/horceracingrecorderandroid/fragment/raceanalysis/RaceAnalysisFragment.kt
@@ -1,4 +1,23 @@
 package com.example.horceracingrecorderandroid.fragment.raceanalysis
 
-class RaceAnalysisFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.horceracingrecorderandroid.databinding.FragmentRaceAnalysisBinding
+
+class RaceAnalysisFragment: Fragment() {
+
+    private lateinit var binding: FragmentRaceAnalysisBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRaceAnalysisBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 }
