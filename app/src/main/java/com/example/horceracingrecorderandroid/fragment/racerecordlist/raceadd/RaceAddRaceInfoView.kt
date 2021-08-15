6666a95a9ad375bbb00051204b91e5d63dc2aed9
subta90 <the.bird.nobility298@gmail.com>
2021-08-15 15:55:46 +0900

    レース名とグレード入力用のレイアウトを追加

--- app/src/main/java/com/example/horceracingrecorderandroid/fragment/racerecordlist/raceadd/RaceAddRaceInfoView.kt
+++ app/src/main/java/com/example/horceracingrecorderandroid/fragment/racerecordlist/raceadd/RaceAddRaceInfoView.kt
@@ -1,4 +1,23 @@
 package com.example.horceracingrecorderandroid.fragment.racerecordlist.raceadd
 
-class RaceAddRaceInfoView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.horceracingrecorderandroid.databinding.ViewRaceAddRaceInfoBinding
+import com.example.horceracingrecorderandroid.enum.GradeType
+
+class RaceAddRaceInfoView(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+
+    private var binding: ViewRaceAddRaceInfoBinding =
+        ViewRaceAddRaceInfoBinding.inflate(LayoutInflater.from(context), this, true)
+
+    init {
+        val gradeAdapter = ArrayAdapter(context, android.R.layout.simple_spinner_item, GradeType.values())
+        binding.raceNameGradeSpinner.adapter = gradeAdapter
+    }
+
+
 }
